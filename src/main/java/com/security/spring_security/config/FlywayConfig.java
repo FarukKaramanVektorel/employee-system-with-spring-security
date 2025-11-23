ab11a29855411c4d5b2c93ef90ddd8215ce2fd79
faruk karaman <farukkaraman@gmail.com>
2025-11-23 13:04:43 +0300

    23-11-2025

---
+++ src/main/java/com/security/spring_security/config/FlywayConfig.java
@@ -0,0 +1,25 @@
+package com.security.spring_security.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+    @Bean
+    public CommandLineRunner flywayMigration(DataSource dataSource) {
+        return args -> {
+            Flyway flyway = Flyway.configure()
+                    .dataSource(dataSource)
+                    .locations("classpath:db/migration")
+                    .baselineOnMigrate(true)
+                    .load();
+            flyway.repair();
+
+            flyway.migrate();
+        };
+    }
+}
